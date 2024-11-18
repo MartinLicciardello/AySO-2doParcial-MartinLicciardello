6f789a28f996a016930c8fdc0c5a94bca81a741f
MartinLicciardello <martinlicciardello7@gmail.com>
2024-11-18 22:08:46 +0000

    punto b del parcial

---
+++ 2doParcial/puntoB.sh
@@ -0,0 +1,23 @@
+#history vmAMN
+ip address show
+ssh-keygen
+cat .ssh/id_rsa.pub
+git clone https://github.com/upszot/UTN-FRA_SO_Ansible.git
+cd UTN-FRA_SO_Ansible/ejemplo_02
+ll .ssh
+nano inventory
+nano playbook.yml
+sudo apt update
+sudo apt install ansible 
+sudo apt list --installed |grep ansible
+ansible-playbook -i inventory playbook.yml
+
+
+#history vmAnsDev  
+
+ip address show
+ll .ssh/
+nano .ssh/authorized_keys
+sudo apt list --installed |grep apache
+history
+
